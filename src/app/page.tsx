4c042de5b1b810e1d65fcb36d695c6776ccf33f9
monkeycs60 <clement.serizay@gmail.com>
2023-03-30 02:35:32 +0200

    🎨 style(.eslintrc.json): remove indent rule 🎨 style(page.tsx): add styling to hello heading and add new paragraph 🎨 style(globals.css): add tailwindcss base, components, and utilities 🎨 style(tailwind.config.js): remove unnecessary content paths The indent rule was removed from the eslint configuration file. The hello heading in the page.tsx file was styled using tailwindcss classes. A new paragraph was added to the page.tsx file. The globals.css file was created and contains the tailwindcss base, components, and utilities. The content paths in the tailwind.config.js file were reduced to only the src directory.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
-import Image from 'next/image';
+import clsx from 'clsx';
 
 export default function Home() {
 	return (
 		<div>
-			<h1>hello</h1>
+			<h1 className={clsx(
+				'text-4xl font-bold text-yellow-600',
+				'sm:text-2xl sm:font-light',
+        'lg:text-6xl lg:font-bold lg:text-pink-600',
+			)} >hello</h1>
+			<p className='text-4xl text-red-500'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam quos, sequi maxime suscipit quod earum illo unde minus perspiciatis impedit.</p>
+      <p>salut les gens</p>
 		</div>
 	);
 }
