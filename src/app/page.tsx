fae2590330a9602f28ade8ba38dacedb92d0176a
monkeycs60 <clement.serizay@gmail.com>
2023-03-30 17:31:04 +0200

    🔥 refactor(page.tsx): remove unused imports and code This commit removes unused imports and code from the page.tsx file. The Link and useRouter imports are no longer used, and the corresponding code that uses them has been removed.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,8 @@
 'use client';
 
 import clsx from 'clsx';
-import Link from 'next/link';
-import { useRouter } from 'next/navigation';
 
 export default function Home() {
-  const router = useRouter();
 	return (
 		<div>
 			<h1 className={clsx(
@@ -13,10 +10,6 @@ export default function Home() {
 				'sm:text-2xl sm:font-light',
         'lg:text-6xl lg:font-bold lg:text-pink-600',
 			)} >hello</h1>
-			<p className='text-4xl text-red-500'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam quos, sequi maxime suscipit quod earum illo unde minus perspiciatis impedit.</p>
-      <p>salut les gens</p>
-      <Link href="/about"> about </Link>
-      <button onClick={() => router.push('/about')}>about</button>
 		</div>
 	);
 }
