7d2a36c736a36ab55262e685682279ad73434ca1
monkeycs60 <clement.serizay@gmail.com>
2023-03-30 23:35:21 +0200

    🚀 chore(deps): add @tanstack/react-query-devtools to package.json 🎨 style(page.tsx): fix indentation in h1 className 🆕 feat(useFetchMusic.tsx): add new file Added a new dependency to the project, @tanstack/react-query-devtools, which will be useful for debugging and monitoring the state of the application. Fixed the indentation in the className of the h1 element in page.tsx. Added a new file, useFetchMusic.tsx, which will contain a custom hook for fetching music data.
    🆕 feat(fetchMusic.tsx): add fetchPlaylist function and MusicScheme
    This commit adds a new file, fetchMusic.tsx, which contains a function named fetchPlaylist that fetches a playlist from the Deezer API and returns an array of MusicType objects. The MusicScheme object is defined using the zod library to validate the shape of the data returned from the API.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
 			<h1 className={clsx(
 				'text-4xl font-bold text-yellow-600',
 				'sm:text-2xl sm:font-light',
-        'lg:text-6xl lg:font-bold lg:text-pink-600',
+        		'lg:text-6xl lg:font-bold lg:text-pink-600',
 			)} >hello</h1>
 		</div>
 	);
