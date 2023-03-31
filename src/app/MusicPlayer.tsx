1abd4c141fe579e20ece5a03154f145bdec40d88
monkeycs60 <clement.serizay@gmail.com>
2023-03-31 02:59:16 +0200

    🎨 style(MusicPlayer.tsx): add title and audio player ✨ feat(MusicPlayer.tsx): add useFetchPlaylist hook The MusicPlayer component now uses the useFetchPlaylist hook to fetch data. The data is logged to the console for debugging purposes. The component now displays a title and an audio player that plays the third song in the fetched data.

--- src/app/MusicPlayer.tsx
+++ src/app/MusicPlayer.tsx
@@ -3,8 +3,15 @@
 import useFetchPlaylist from '@/hooks/useFetchPlaylist';
 
 export const MusicPlayer = () => {
-    useFetchPlaylist();
+    const { data, error, isLoading } = useFetchPlaylist();
+    console.log(data);
+    
   return (
-    <div>MusicPlayer</div>
+    <div>
+        <h1 className='text-4xl text-blue-900'>Music Player</h1>
+        <p>coucou</p>
+        { data &&  <audio controls loop src={data[2].preview}></audio>}
+ 
+    </div>
   );
 };
